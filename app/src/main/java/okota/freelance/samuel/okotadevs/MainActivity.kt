7525eed01450cdad2a7806ed23b06c55523365b5
Samuel Agbede <samuel.agbede@iisysgroup.com>
2018-06-30 12:39:17 +0100

    Added new devs

--- app/src/main/java/okota/freelance/samuel/okotadevs/MainActivity.kt
+++ app/src/main/java/okota/freelance/samuel/okotadevs/MainActivity.kt
@@ -2,11 +2,13 @@ package okota.freelance.samuel.okotadevs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import okota.freelance.samuel.okotadevs.data.UserGenerator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- app/src/main/java/okota/freelance/samuel/okotadevs/data/UserGenerator.kt
+++ app/src/main/java/okota/freelance/samuel/okotadevs/data/UserGenerator.kt
@@ -6,7 +6,24 @@ object UserGenerator {
         with (list){
             val user = Users("Samuel Agbede", "09055052360", "I'm an android developer who loves building worthy solutions")
 
+            val femi = Users("Femi Sotonwa", "07019103894", "Software Developer")
+            val chibuzor = Users("Chibuzor Ojukwu", "07018606789","Software Developer")
+            val oluwole = Users("Oluwole Ilesanmi", "07030565701", "Software Developer")
+
+            val clifford = Users("Clifford Odoh", "08169794997", "Software Developer")
+
+            val henry = Users("Henry Ezeagu", "07064649056", "Software Developer")
+
+            val ifunanya = Users("Ifunanya Albert", "08165640882", "Software Developer")
+
             add(user)
+            add(femi)
+            add(chibuzor)
+            add(oluwole)
+            add(clifford)
+            add(henry)
+            add(ifunanya)
+
         }
 
         return list
